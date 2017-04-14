f3a089039f0f2adabd49f933d5c3c1c9f0d2fcd2
Max Risuhin <risuhin.max@gmail.com>
2017-04-14 10:27:57 +0300

    Build static libs

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,12 @@
 
 mkdir build_release
 cd build_release
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DINSTALL_HEADERS=on -DBUILD_SHARED_LIBS=on -DBUILD_TESTING=on
+cmake ..  \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DINSTALL_HEADERS=on \
+    -DBUILD_SHARED_LIBS=on \
+    -DBUILD_STATIC_LIBS=on \
+    -DBUILD_TESTING=on
 make
 ctest
 make install
