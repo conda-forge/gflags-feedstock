5a82472b7456beacf5bb5ee95018a1271c150e3b
Jonathan Helmus <jjhelmus@gmail.com>
2017-07-07 07:15:24 -0500

    Add .so.2 library files on Linux
    Some downstream packages including caffe like against these files and break
    when they are not present.  On macOS no packages are known which used the .2
    libraries and therefore they are not created.
    
    closes #9

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,3 +11,12 @@ cmake ..  \
 make
 ctest
 make install
+
+if [[ `uname` == Linux ]]; then
+    # Create the *.so.2 shared libraries needed by downstreammpackages (caffe).
+    # Similar files are not needed on macOS as no downstream packages have been
+    # found which depend on them.
+    cd $PREFIX/lib
+    ln -s libgflags.so.2.2.0 libgflags.so.2
+    ln -s libgflags_nothreads.so.2.2.0 libgflags_nothreads.so.2
+fi
